2101c99d192afaf5a3b53be74d4afee2b2b565c1
Artem Chymshyr <artem.chymshyr@gmail.com>
2025-12-10 20:30:37 +0100

    Initialization

---
+++ src/main/java/org/example/pomodorobuddy/PomodoroBuddyApplication.java
@@ -0,0 +1,13 @@
+package org.example.pomodorobuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PomodoroBuddyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PomodoroBuddyApplication.class, args);
+    }
+
+}
