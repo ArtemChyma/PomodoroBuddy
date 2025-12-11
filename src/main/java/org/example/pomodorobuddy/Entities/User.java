96b58eab067f0d0addcb87a254b1177f9c6491e7
Artem Chymshyr <artem.chymshyr@gmail.com>
2025-12-11 20:56:07 +0100

    added User entity class

---
+++ src/main/java/org/example/pomodorobuddy/Entities/User.java
@@ -0,0 +1,25 @@
+package org.example.pomodorobuddy.Entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long Id;
+    @Column(name = "email_address", unique = true, nullable = false)
+    private String email;
+    @Column(name = "password")
+    private String password;
+
+    public User(){}
+
+    public User(Long id, String email, String password) {
+        Id = id;
+        this.email = email;
+        this.password = password;
+    }
+
+}
